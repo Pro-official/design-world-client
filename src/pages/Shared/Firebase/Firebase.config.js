63020f96c463fe7c23d2ed8761165ee5103f15aa
Pro-official <promise.iam00@gmail.com>
2021-12-25 03:54:05 +0600

    design improved and hosted in vercel

--- src/pages/Shared/Firebase/Firebase.config.js
+++ src/pages/Shared/Firebase/Firebase.config.js
@@ -5,6 +5,13 @@ const firebaseConfig = {
   storageBucket: "design-world-official.appspot.com",
   messagingSenderId: "747177883634",
   appId: "1:747177883634:web:68b231148f858fc1f49d6b",
+  // apiKey: "AIzaSyDOiS9H5IrDakiq7syFG2bIiOud3sNICIg",
+  // authDomain: "world-design-official.firebaseapp.com",
+  // projectId: "world-design-official",
+  // storageBucket: "world-design-official.appspot.com",
+  // messagingSenderId: "141997391397",
+  // appId: "1:141997391397:web:5aa3fe706a911ad2fc09cc",
+  // measurementId: "G-TB926J1E59",
 };
 
 export default firebaseConfig;
